59cb69ce5552f5e8dc4f03aeebf4b07e5122195d
Tiep Phan <tieppt12@gmail.com>
2020-01-10 01:15:42 +0700

    test functions

--- src/lambda/lambda.js
+++
@@ -1,8 +0,0 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const server = require('../dist/server');
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
-
-server.app.use(awsServerlessExpressMiddleware.eventContext());
-
-const serverProxy = awsServerlessExpress.createServer(server.app);
-module.exports.ssrserverless = (event, context) => awsServerlessExpress.proxy(serverProxy, event, context);
