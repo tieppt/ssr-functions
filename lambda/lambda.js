a87b83aa99d46f5da827e95050f928eb884ca7fb
Tiep Phan <tieppt12@gmail.com>
2020-01-10 00:49:02 +0700

    test 2

--- lambda/lambda.js
+++ lambda/lambda.js
@@ -1,5 +1,5 @@
 const awsServerlessExpress = require('aws-serverless-express');
-const server = require('./dist/server');
+const server = require('../dist/server');
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
 server.app.use(awsServerlessExpressMiddleware.eventContext());
