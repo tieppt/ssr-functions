c52e4b762535c6813e34b271447b20d35c4fe469
Tiep Phan <tieppt12@gmail.com>
2020-01-10 01:23:44 +0700

    update ssr functions

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -9,9 +9,9 @@ module.exports = {
     // This is our Express server for Dynamic universal
     server: './server.ts'
   },
-  externals: {
-    './dist/server/main': 'require("./server/main")'
-  },
+  // externals: {
+  //   './dist/server/main': 'require("./server/main")'
+  // },
   target: 'node',
   resolve: { extensions: ['.ts', '.js'] },
   optimization: {
@@ -19,6 +19,7 @@ module.exports = {
   },
   output: {
     // Puts the output at the root of the dist folder
+    libraryTarget: 'commonjs',
     path: path.join(__dirname, 'dist'),
     filename: '[name].js'
   },
