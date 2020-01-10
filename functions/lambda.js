bf4681664eb0f6f9b9ab695ba9ae17f2ef1b9eeb
Tiep Phan <tieppt12@gmail.com>
2020-01-10 10:36:22 +0700

    update path to server ssr

--- functions/lambda.js
+++ functions/lambda.js
@@ -1,5 +1,5 @@
 const awsServerlessExpress = require('aws-serverless-express');
-const server = require(`${process.cwd()}/dist/server`);
+const server = require(`${process.cwd()}/server`);
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
 server.app.use(awsServerlessExpressMiddleware.eventContext());
